f8d91ae22019ebcd1315e3eceec402748cd82c17
chenxie95 <chenxie95@gmail.com>
2021-03-26 17:24:29 -0700

    gggg

--- app_orig.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 3000
-app.get('/', (req, res) => res.send("Hello World, Xie, current port " + port));
-app.listen(port, () => console.log('Server is running on port ', port));
-console.log('finally arrive here, let us continue...');
