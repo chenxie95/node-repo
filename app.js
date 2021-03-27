ad45dba7ab2332ba684578228b06eafe5bcbe9fd
chenxie95 <chenxie95@gmail.com>
2021-03-26 18:37:27 -0700

    rrrr

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000
+app.get('/', (req, res) => res.send("Hello World, Xie, current port " + port + "this is in index.js"));
+app.listen(port, () => console.log('Server is running on port ', port));
+console.log('finally arrive here, let us continue...');
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 3000
-app.get('/', (req, res) => res.send("Hello World, Xie, current port " + port + "this is in index.js"));
-app.listen(port, () => console.log('Server is running on port ', port));
-console.log('finally arrive here, let us continue...');
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function() {
+    console.log('listening to requests on port 4000')
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data)
+    });
+
+});
+
