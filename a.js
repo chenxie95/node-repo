37bb073c0501a50b9bb22563579ff46cd52209be
chenxie95 <chenxie95@gmail.com>
2021-03-26 23:22:20 -0700

    tidy update the code

--- a.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 3000
-app.get('/', (req, res) => res.send("Hello World, Xie, current port " + port + "this is in a.js"));
-app.listen(port, () => console.log('Server is running on port ', port));
-console.log('finally arrive here, let us continue...');
